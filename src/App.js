6d28cf4781fe60a38f4dbf8f076dfdf323e729a5
marie <marielee14@gmail.com>
2022-11-03 17:38:31 +0900

    react study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { getImageUrl } from "./Profile.js";
 
-function App() {
+export default function Gallery() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notable Scientists</h1>
+      <section className="profile">
+        <h2>Maria Skłodowska-Curie</h2>
+        <img
+          className="avatar"
+          src={getImageUrl("szV5sdG")}
+          alt="Maria Skłodowska-Curie"
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            physicist and chemist
+          </li>
+          <li>
+            <b>Awards: 4 </b>
+            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal,
+            Matteucci Medal)
+          </li>
+          <li>
+            <b>Discovered: </b>
+            polonium (element)
+          </li>
+        </ul>
+      </section>
+      <section className="profile">
+        <h2>Katsuko Saruhashi</h2>
+        <img
+          className="avatar"
+          src={getImageUrl("YfeOqp2")}
+          alt="Katsuko Saruhashi"
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            geochemist
+          </li>
+          <li>
+            <b>Awards: 2 </b>
+            (Miyake Prize for geochemistry, Tanaka Prize)
+          </li>
+          <li>
+            <b>Discovered: </b>a method for measuring carbon dioxide in seawater
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
-
-export default App;
---
+++ src/Gallery.js
@@ -0,0 +1,12 @@
+import Profile from "./Profile";
+
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Amazing Scientists</h1>
+      L<Profile />
+      L<Profile />
+      L<Profile />
+    </div>
+  );
+}
